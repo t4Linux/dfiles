6264ebc98a04cece09745385187ed31a4ad4873b
t4Linux <subs@kalel.pl>
2023-03-01 19:03:02 +0100

    estetc changes

--- zsh/settings/editor_settings.zsh
+++ zsh/settings/editor_settings.zsh
@@ -1,4 +1,3 @@
-#!/bin/env bash
 # +-- created by: achmur -------------- time4Linux ------------------------+ #
 # |     __  __ __  __    _                           __           .--.     | #
 # |    / /_/ // / / /   (_)___  __  ___  __   ____  / /          |ö_ö |    | #
@@ -6,14 +5,14 @@
 # |  / /_/__  __/ /___/ / / / / /_/ />  < _ / /_/ / /           //   \ \   | #
 # |  \__/  /_/ /_____/_/_/ /_/\__,_/_/|_/(_) .___/_/           (|     | )  | #
 # |                                       /_/                 /'\_   _/`\\ | #
-# |  Dscr: ZSH configuration - KUBERNETES      2022-12-13     \___)=(___// | #
-# +------------------------------------------ 2023-03-01 ------------------+ #
-#
-if command -v kubectl &> /dev/null; then
-  autoload -Uz +X compinit && compinit
-  source <(kubectl completion zsh | sed 's#${requestComp} 2>/dev/null#${requestComp} 2>/dev/null | head -n -1 | fzf  --multi=0 #g')
-  alias kubectl=kubecolor 1>/dev/null
-  compdef __start_kubectl k
-  compdef kubecolor=kubectl
-fi
+# |  Description: ZSH configuration -editor    2023-02-17     \___)=(___// | #
+# +------------------------------------------ 2023-02-23 ------------------+ #
 
+MY_FILES="$HOME/dotfiles/zsh/plugins"
+#------------------- BAT --------------------------#
+export BAT_THEME="Coldark-Dark"
+export BAT_STYLE="header-filename,header-filesize,grid"
+#------------------ EDITOR ------------------------#
+[[ ! -d $MY_FILES ]] || \
+(source $MY_FILES/editor.zsh && \
+ source $MY_FILES/run_editor)
