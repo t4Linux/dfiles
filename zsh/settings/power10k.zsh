2cf73d7e862fe53e4a06c5756451fd56f1581659
t4Linux <subs@kalel.pl>
2023-09-14 23:22:24 +0200

    p10k fix

--- zsh/settings/power10k.zsh
+++ zsh/settings/power10k.zsh
@@ -13,6 +13,6 @@
 [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]] &&
 . "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
 #   To customize prompt, run `p10k configure` or edit p10k.zsh 
-[[ ! -f $MY_FILES/zsh/plugins/p10k.zsh ]] || . ~/.config/zsh/plugins/p10k.zsh
+. $MY_FILES/zsh/plugins/p10k.zsh
 . $MY_FILES/zsh/powerlevel10k/powerlevel10k.zsh-theme
 
