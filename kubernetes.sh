94509359f3ce2d862a0492fb3beb133d1e67acd4
t4Linux <subs@kalel.pl>
2023-09-20 21:35:13 +0200

    kubernetes

---
+++ kubernetes.sh
@@ -0,0 +1,10 @@
+#!/bin/env bash
+
+. ~/config_env
+# krew plugin for kubectl
+ln -s $MY_FILES/krew/ $HOME/.krew
+export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"
+
+# color output for command
+sudo apt update && sudo apt install nala -y
+sudo nala install kubecolor
